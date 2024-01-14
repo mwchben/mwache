7fccddaeefa422201f5e170b29da99400d206a70
Benjamin Chege <benjamwa05@gmail.com>
2024-01-14 11:27:37 +0300

    mod in copyright

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,15 +1,12 @@
 import * as React from "react";
-import { Typography, Link, Chip, Divider } from "@mui/material";
+import { Typography, Link } from "@mui/material";
 
 function Copyright(props) {
   return (
     <>
-      <Divider sx={{ my: 3 }} light={true}>
-        <Chip label="🙂🙂🙂🙂🙂🙂" size="small" />
-      </Divider>
       <Typography variant="body2" color="#e4e6eb" align="center" {...props}>
         {"Copyright © "}
-        <Link color="inherit" href="https://github/mwchben.com/">
+        <Link color="inherit" href="https://github.com/mwchben">
           Mwache
         </Link>{" "}
         {new Date().getFullYear()}

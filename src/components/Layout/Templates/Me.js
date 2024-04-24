6232a259dcac7ab12ef7419ecfc74e1a77a33ddd
Benjamin Mwangi <benjamwa05@gmail.com>
2024-04-24 17:55:18 +0300

    the me part as a component

---
+++ src/components/Layout/Templates/Me.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+function Me() {
+ 
+  return (
+    <Grid item>
+      
+            <Typography component="h2" variant="h5">
+              Bleh
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+            54/54/2014
+            </Typography>
+            <Typography variant="subtitle1" paragraph color="text.secondary" >
+            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. 
+            Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, 
+            tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. 
+                Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. 
+                Etiam porta sem 
+            </Typography>
+         
+
+    </Grid>
+  );
+}
+
+
+export default Me;

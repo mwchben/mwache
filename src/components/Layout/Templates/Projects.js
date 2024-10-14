cc6d4c6a7f741c68ad5b0594dece3af31e4a9a92
Benjamin Mwangi <benjamwa05@gmail.com>
2024-10-14 18:08:34 +0300

    removed my projects

--- src/components/Layout/Templates/Projects.js
+++ src/components/Layout/Templates/Projects.js
@@ -3,45 +3,37 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Me from './Me';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
-import IconButton from '@mui/material/IconButton';
-import LaunchRoundedIcon from '@mui/icons-material/LaunchRounded';
-import { Box, Link } from '@mui/material';
 
 
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Lio Website',
-    link: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Dashboard',
-    link: '@hjrc33',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Mobile integration',
-    link: `https://images.unsplash.com/photo-1558642452-9d2a7deb7f62`,
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Available on all platforms',
-    link: '@tjdragotta',
-  } 
-];
+// const itemData = [
+//   {
+//     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+//     title: 'Lio Website',
+//     link: '@bkristastucchio',
+//     rows: 2,
+//     cols: 2,
+//     featured: true,
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+//     title: 'Dashboard',
+//     link: '@hjrc33',
+//     cols: 2,
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+//     title: 'Mobile integration',
+//     link: `https://images.unsplash.com/photo-1558642452-9d2a7deb7f62`,
+//     rows: 2,
+//     cols: 2,
+//     featured: true,
+//   },
+//   {
+//     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+//     title: 'Available on all platforms',
+//     link: '@tjdragotta',
+//   } 
+// ];
 
 export default function Projects() {
  
@@ -94,65 +86,7 @@ export default function Projects() {
 
 {/* ............................RIGHT.................sx={{ width: 500, height: 450 }}... */}
             <Grid item >
-              <ImageList >
-                <ImageListItem key="Subheader" cols={2}>
-                  <ListSubheader component="div">My Projects</ListSubheader>
-                </ImageListItem>
-                {itemData.map((item) => (
-                  <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-between',
-                    gap: 1,
-                    height: '100%',
-                  }}
-                  >
-                  {/* <ImageListItem key={item.img} xs={12} md={6}>
-                    <img
-                      srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                      src={`${item.img}?w=248&fit=crop&auto=format`}
-                      alt={item.title}
-                      loading="lazy"
-                    />
-                    <ImageListItemBar
-                      title={item.title}
-                      subtitle={item.link}
-                      actionIcon={
-                        <IconButton
-                          sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                          aria-label={`info about ${item.title}`}
-                        >
-                          <LaunchRoundedIcon />
-                        </IconButton>
-                      }
-                    />
-                  </ImageListItem> */}
-                   <Typography gutterBottom variant="caption" component="div">
-                {article.tag}
-              </Typography>
-              <TitleTypography
-                gutterBottom
-                variant="h6"
-                onFocus={() => handleFocus(index)}
-                onBlur={handleBlur}
-                tabIndex={0}
-                className={focusedCardIndex === index ? 'Mui-focused' : ''}
-              >
-                {article.title}
-                <NavigateNextRoundedIcon
-                  className="arrow"
-                  sx={{ fontSize: '1rem' }}
-                />
-              </TitleTypography>
-              <StyledTypography variant="body2" color="text.secondary" gutterBottom>
-                {article.description}
-              </StyledTypography>
-
-              <Author authors={article.authors} />
-                  </Box>
-                ))}
-              </ImageList>
+                
               
             </Grid>
 
@@ -163,3 +97,5 @@ export default function Projects() {
     </>
   );
 }
+
+
